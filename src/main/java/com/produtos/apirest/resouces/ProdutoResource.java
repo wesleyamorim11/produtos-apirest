a0817dedaacba50bd0d50587e958abe67f584b5b
Wesley Amorim <wesleyamorim290@gmail.com>
2021-04-20 22:40:15 -0300

    Teste

--- src/main/java/com/produtos/apirest/resouces/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resouces/ProdutoResource.java
@@ -3,7 +3,11 @@ package com.produtos.apirest.resouces;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +25,22 @@ public class ProdutoResource {
 	public List<Produto> listaProdutos(){
 	return produtoRepository.findAll();
 	}
-
-	// http://localhost:8080/api/produtos
+	
+	@GetMapping("/produto{id}")
+	public Produto listaProdutoPorId(@PathVariable(value="id")long id) {
+		return produtoRepository.findById(id);
 	}
+
+	@PostMapping("/produto")
+	public Produto incluirProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	public void deletarProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+
+}
 
